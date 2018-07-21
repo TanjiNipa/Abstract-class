113008195ce69972bd210631bf4b0e2dd7a5b705
Nipa <tanjinidhe777@gmail.com>
2018-07-21 03:22:17 -0700

    Abstract code!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,33 @@
 package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        nipaImpl nipa = new nipaImpl();
-        trisha trisha = new trisha();
-        Fahmida fahmida = new Fahmida();
+       //* nipaImpl nipa = new nipaImpl();
+        //trisha trisha = new trisha();
+        //Fahmida fahmida = new Fahmida();
+
+        //nipa.sleep();
+        //nipa.works();
+
+        //trisha.sleep();
+        //trisha.works();
+
+        //fahmida.sleep();
+       // fahmida.works();
+        human nipa = new nipaImpl();
+        human banu = new human() {
+            @Override
+            void works() {
+                System.out.println("Clean ");
+            }
+        };
 
         nipa.sleep();
         nipa.works();
 
-        trisha.sleep();
-        trisha.works();
 
-        fahmida.sleep();
-        fahmida.works();
+        banu.sleep();
+        banu.works();
     }
 }
