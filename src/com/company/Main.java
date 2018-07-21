e2ddaaeaee9e6986887ec4417dd426fcb6e2d818
Nipa <tanjinidhe777@gmail.com>
2018-07-21 03:04:02 -0700

    Abstract code!

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        nipaImpl nipa = new nipaImpl();
+        trisha trisha = new trisha();
+        Fahmida fahmida = new Fahmida();
+
+        nipa.sleep();
+        nipa.works();
+
+        trisha.sleep();
+        trisha.works();
+
+        fahmida.sleep();
+        fahmida.works();
+    }
+}
